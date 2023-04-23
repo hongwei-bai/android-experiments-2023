89ab57496d5b15cb88e124aba50b281e6e84d42c
Mike <bhw8412@hotmail.com>
2023-04-23 16:49:45 +1000

    upgrade libs

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,14 @@
+object Versions {
+    const val kotlin = "1.8.10"
+    const val kotlinCompilerExtension = "1.4.4"
+    const val compose = "1.4.2"
+    const val ktCoil = "1.3.2"
+    const val hilt = "1.0.0"
+    const val hiltAndroidPlugin = "2.45"
+    const val coroutines = "1.6.4"
+    const val composeAccompanist = "0.28.0"
+    const val activityCompose = "1.5.1"
+    const val lifeCycle = "2.3.1"
+    const val navigationCompose = "2.5.3"
+    const val lottieCompose = "6.0.0"
+}
